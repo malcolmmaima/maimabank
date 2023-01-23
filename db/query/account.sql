34fae91d41b955053e351548a89fbd8bb9f051f0
malcolm maima <malcolmmaima@gmail.com>
2023-01-23 13:31:36 +0300

    updateAccount sql generate

--- db/query/account.sql
+++ db/query/account.sql
@@ -12,3 +12,6 @@ SELECT * FROM accounts WHERE id = $1 LIMIT 1;
 
 -- name: ListAccounts :many
 SELECT * FROM accounts ORDER BY id LIMIT $1 OFFSET $2;
+
+-- name: UpdateAccount :exec
+UPDATE accounts SET balance = $2 WHERE id = $1;
