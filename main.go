c1c76dfc8c50c5931d549965a272dc7cb9559733
malcolm maima <malcolmmaima@gmail.com>
2023-02-16 21:57:38 +0300

    setup server

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+	db "github.com/malcolmmaima/maimabank/db/sqlc"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:7915@localhost:5432/maima_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+
+	store := db.NewStore(conn)
+	server := server.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
+}
