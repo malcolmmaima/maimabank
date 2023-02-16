9db7dfc6a160817f5e9383e98d27d1f9b17388bc
malcolm maima <malcolmmaima@gmail.com>
2023-02-16 22:44:27 +0300

    get accounts

--- api/server.go
+++ api/server.go
@@ -17,7 +17,7 @@ func NewServer(store *db.Store) *Server {
 	router := gin.Default()
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
-	// router.GET("/accounts", server.listAccounts)
+	router.GET("/accounts", server.listAccounts)
 	server.router = router
 	return server
 }
