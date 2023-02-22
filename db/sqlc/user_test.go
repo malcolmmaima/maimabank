67054d93c872b2dad21a1467cd63ab483c35a266
malcolm maima <malcolmmaima@gmail.com>
2023-02-22 22:21:09 +0300

    create user request

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -16,9 +16,12 @@ func TestCreateUser(t *testing.T) {
 
 
 func createRandomUser(t *testing.T) User {
+	hashPassword, err := util.HashPassword(util.RandomString(6))
+	require.NoError(t, err)
+	
 	arg := CreateUserParams{
 		Username: util.RandomOwner(),
-		HashedPassword: "secret",
+		HashedPassword: hashPassword,
 		FullName: util.RandomOwner(),
 		Email: util.RandomEmail(),
 	}
--- util/password_test.go
+++ util/password_test.go
@@ -10,14 +10,20 @@ import (
 func TestPassword(t *testing.T) {
 	password := RandomString(6)
 
-	hashPassword, err := HashPassword(password)
+	hashPassword1, err := HashPassword(password)
 	require.NoError(t, err)
-	require.NotEmpty(t, hashPassword)
+	require.NotEmpty(t, hashPassword1)
 
-	err = CheckPassword(password, hashPassword)
+	err = CheckPassword(password, hashPassword1)
 	require.NoError(t, err)
 
 	wrongPassword := RandomString(6)
-	err = CheckPassword(wrongPassword, hashPassword)
+	err = CheckPassword(wrongPassword, hashPassword1)
 	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+
+	// two similar passwords should not have the same hashpasswordS
+	hashPassword2, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashPassword2)
+	require.NotEqual(t, hashPassword1, hashPassword2)
 }
