9e0ca1496dad02337d630a3e93a5a0723dc6eb9a
malcolm maima <malcolmmaima@gmail.com>
2023-07-20 12:28:19 +0300

    Create jwt_maker_test.go

---
+++ token/jwt_maker_test.go
@@ -0,0 +1,53 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/malcolmmaima/maimabank/util"
+	"github.com/stretchr/testify/require"
+)
+
+// Happy path
+func TestJWTMaker(t *testing.T) {
+	maker, err := NewJWTMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomOwner()
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+
+	require.WithinDuration(t, expiredAt, payload.ExpiresAt, time.Second)
+}
+
+// Sad path
+func TestExpiredJWTToken(t *testing.T) {
+	maker, err := NewJWTMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomOwner()
+	duration := -time.Minute
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
