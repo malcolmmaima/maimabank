cd1196a967a9c7bba281a15e285051ecfe809ee9
malcolm maima <malcolmmaima@gmail.com>
2023-07-21 11:53:05 +0300

    Update middleware.go

--- api/middleware.go
+++ api/middleware.go
@@ -7,13 +7,14 @@ import (
 	"github.com/malcolmmaima/maimabank/token"
 )
 
+// Constants for authorization header keys and types.
 const (
 	authorizationHeaderKey = "authorization"
 	authorizationTypeBearer = "bearer"
 	authorizationPayloadKey = "authorization_payload"
 )
-	
 
+// authMiddleware is a middleware function that performs token authentication.
 func authMiddleware(tokenMaker token.Maker) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		// Get authorization header
@@ -23,26 +24,34 @@ func authMiddleware(tokenMaker token.Maker) gin.HandlerFunc {
 			return
 		}
 
+		// Split authorization header into fields (type and token)
 		fields := strings.Fields(authorizationHeader)
 		if len(fields) < 2 {
 			ctx.AbortWithStatusJSON(401, gin.H{"error": "invalid authorization header format"})
 			return
 		}
 
+		// Extract the authorization type and check if it is "bearer"
 		authorizationType := strings.ToLower(fields[0])
 		if authorizationType != authorizationTypeBearer {
 			ctx.AbortWithStatusJSON(401, gin.H{"error": "unsupported authorization type"})
 			return
 		}
 
+		// Extract the access token from the authorization header
 		accessToken := fields[1]
+
+		// Verify the access token using the token maker
 		payload, err := tokenMaker.VerifyToken(accessToken)
 		if err != nil {
 			ctx.AbortWithStatusJSON(401, gin.H{"error": err.Error()})
 			return
 		}
 
+		// Set the authorization payload in the context for further use
 		ctx.Set(authorizationPayloadKey, payload)
+
+		// Call the next middleware or handler in the chain
 		ctx.Next()
 	}
 }
