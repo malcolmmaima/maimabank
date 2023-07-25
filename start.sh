84f46d8e4b8cbeaef398d7e50fe236e0b6bc15dd
malcolm maima <malcolmmaima@gmail.com>
2023-07-25 12:54:19 +0300

    fix docker issues

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "run db migrations"
-/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+/app/migrate -path /app/db/migration -database "$DB_SOURCE" -verbose up
 
 echo "Starting the application..."
 ./main
