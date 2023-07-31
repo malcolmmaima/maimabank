ffa572341c12c2d0b50798aef7d9acca32738d8a
malcolm maima <malcolmmaima@gmail.com>
2023-07-31 15:26:32 +0300

    generate go sqlc queries

---
+++ db/query/exchange_rates.sql
@@ -0,0 +1,26 @@
+-- name: CreateExchangeRate :one
+INSERT INTO exchange_rates (
+  base_currency,
+  target_currency,
+  exchange_rate
+) VALUES (
+  $1, $2, $3
+) RETURNING *;
+
+-- name: GetExchangeRate :one
+SELECT * FROM exchange_rates
+WHERE id = $1 LIMIT 1;
+
+-- name: ListExchangeRates :many
+SELECT * FROM exchange_rates
+WHERE base_currency = $1;
+
+-- name: UpdateExchangeRate :one
+UPDATE exchange_rates SET
+  exchange_rate = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteExchangeRate :exec
+DELETE FROM exchange_rates
+WHERE id = $1;
