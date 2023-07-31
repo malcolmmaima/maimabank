45d61b0f933ef8a29393b93c7b84251c89004ac4
malcolm maima <malcolmmaima@gmail.com>
2023-07-31 15:07:54 +0300

    add migration

---
+++ db/migration/000004_add_exchange_rates.down.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS "exchange_rates";
---
+++ db/migration/000004_add_exchange_rates.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "exchange_rates" (
+  "id" bigserial PRIMARY KEY,
+  "base_currency" varchar NOT NULL,
+  "target_currency" varchar NOT NULL,
+  "exchange_rate" decimal NOT NULL,,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE INDEX ON "exchange_rates" ("base_currency");
+
+CREATE INDEX ON "exchange_rates" ("target_currency");
